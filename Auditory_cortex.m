69c1f0335ec1f1ff6854e097917bc49babebdaf0
felipe89duque <felipe89.duque@hotmail.com>
2022-02-17 14:55:16 +0000

    Updated model
    Ready for testing and plotting the article's figures. To this point, figures 2, 3 and 4 can be plotted

--- Auditory_cortex.m
+++ Auditory_cortex.m
@@ -5,9 +5,9 @@ clear all; clc; close all;
 % https://doi.org/10.3389/neuro.01.1.1.015.2007
 
 %% Parameters
-columns = 5; %15;
-cells_per_column = 4;%200;
-N_E = 2;%100;
+columns = 15;
+cells_per_column = 200;
+N_E = 100;
 N_I = cells_per_column - N_E;
 
 Tau_E = 0.001; % s
@@ -38,15 +38,12 @@ params.J_EI = J_EI;
 params.J_IE = J_IE;
 params.J_II = J_II;
 params.U = U;
-params.Taur_rec = Tau_rec;
+params.Tau_rec = Tau_rec;
 
 %% External input
 % background activity
 e_max = 10; % Hz
 e_min = -10; % Hz
-
-% stimulus
-s = 0;
 
 % Create background noise
 e_E = zeros(N_E,columns); % Hz
@@ -71,35 +68,190 @@ for ii = 1:columns
     e_I(:,ii) = sort(actual_eI);
 end
 
-%% Create neurons
-E = rand(N_E,columns);
-I = rand(N_I,columns);
 
-%% Create synaptic resources
-x = rand(size(E));
-y = rand(size(I));
+% stimulus
+s_params.alpha = 2;
+s_params.lambda_c = 0.25;
+s_params.delta_left = 5;
+s_params.delta_right= 5;
 
-%% Get initial conditions
-% set stimulus to 0 and let the system converge
-X0 = [E(:); I(:); x(:); y(:)];
-t_rest = 5*Tau_rec;
-[t, X] = ode45(@(t,y) dXdt(t,y,params,0,e_E,e_I),[0,t_rest],X0);
+%% Figure 2
+% start_time = 0.1; % s 
+% stimuli_durations = [0.4]; % how long each stimulus lasts
+% s = zeros(max(size(stimuli_durations)),columns);
+% s(1,:) = [0,0,0,0,0,0,0,4,0,0,0,0,0,0,0];
+% s = compute_s_tuning_curve(s,s_params);
+% 
+% [t,E,I,x,y] = solve_complex_stimuli(start_time,stimuli_durations,s,params,e_E,e_I);
+% % plot average activity per column
+% E_col_avg = mean(E,3);
+% 
+% t = 1000*(t-start_time); % scale to milliseconds, and offset so that the first stimulus is at t=0
+% max_amplitude = max(E_col_avg,[],'all');
+% for col =6:10
+%     subplot(3,5,col-5);
+%     plot(t,E_col_avg(:,col),'DisplayName',sprintf('col %d',col));
+%     ylim([0,max_amplitude]);
+%     
+%     subplot(3,5,col);
+%     plot(t,s(col)*heaviside(t)); % The temporal envelope zeta used, is the heaviside function
+%     ylim([0,max(s)+1]);
+%     
+% end
+% subplot(3,5,[11,12,13,14,15]);
+% plot(6:10,s(6:10));
+% xticks([6,7,8,9,10]);
 
-[E,I,x,y] = reshape_X(X,[2,3,1], params);
 
-E0 = E(:,:,end);
-I0 = I(:,:,end);
-x0 = x(:,:,end);
-y0 = y(:,:,end);
+%% Figure 3
+% start_time = 0.025; % s 
+% stimuli_durations = [0.075]; % how long each stimulus lasts
+% s = zeros(max(size(stimuli_durations)),columns);
+% 
+% min_input = 2;
+% max_input = 8;
+% for ii = min_input:max_input % arbitrary increasing stimulus
+%     s(1,:) = [0,0,0,0,0,0,0,ii,0,0,0,0,0,0,0];
+%     s = compute_s_tuning_curve(s,s_params);
+% 
+%     [t,E,I,x,y] = solve_complex_stimuli(start_time,stimuli_durations,s,params,e_E,e_I);
+%     % average activity per column
+%     E_col_avg = mean(E,3);
+%     
+%     x_axis = 1:size(E_col_avg,2)+1; %columns (+1 so that the last column appears interpolated from last_column to last_column+1)
+%     y_axis = t;
+%     [X_axis,Y_axis] = meshgrid(x_axis,y_axis);
+%     subplot(max_input+1,1,max_input-ii+1);
+%     grid off;
+%     surf(X_axis-0.5,Y_axis,cat(2,E_col_avg,zeros(size(t,1),1)),'edgecolor','none');
+%     xlim([0.5,15.5]);
+%     ylim([0,start_time+stimuli_durations(1)]);
+%     xticks([]);
+% %     colorbar;
+%     view(2);
+%     subplot(max_input+1,1,max_input+1);
+%     plot(1:columns,s);
+%     xlim([0.5,15.5]);
+%     xticks([1:columns]);
+%     hold on;
+% end
 
-%% Solve ODE
-t_span = [0, 5*Tau_rec];
-X0 = [E0(:); I0(:); x0(:); y0(:)];
-[t, X] = ode45(@(t,y) dXdt(t,y,params,s,e_E,e_I),t_span,X0);
+%% Figure 4
+start_time = 0; % s 
+stimuli_durations = [0.075]; % how long each stimulus lasts
 
-[E,I,x,y] = reshape_X(X,[1,3,2], params);
+max_amp = 10;
+step = 1;
+for tone_freq = 8:2:12
+    t_peak_vec = zeros(1,1/step*max_amp);
+    cont = 1;
+    for tone_amp = 1:step:max_amp
+        s = zeros(max(size(stimuli_durations)),columns);
+        s(1,tone_freq) = tone_amp;
+        s = compute_s_tuning_curve(s,s_params);
+        [t,E,I,x,y] = solve_complex_stimuli(start_time,stimuli_durations,s,params,e_E,e_I);
+        % average activity per column
+        E_col_avg = mean(E,3);
+        I_col_avg = mean(I,3);
+        
+        E_col8 = E_col_avg(:,8);
+        I_col8 = I_col_avg(:,8);
+        
+        [~,E_indices]=findpeaks(E_col8,'MinPeakHeight', 15); %Completely arbitrary threshold O_O
+        [~,I_indices]=findpeaks(I_col8,'MinPeakHeight', 15);         
+        time_E_peak = t(E_indices);
+        time_I_peak = t(I_indices);
+        
+        if isempty(time_E_peak)
+            t_peak_vec(cont) = nan;
+        else
+            t_peak_vec(cont) = time_E_peak;            
+        end
+        cont = cont+1;
+    end
+    hold on;
+    plot(1:step:max_amp,1000*t_peak_vec,'.k');
+end
 
+ylim([0,40]);
+xlim([0,10]);
+%% Functions
+function [t_vec,E,I,x,y] = solve_complex_stimuli(start_time,dur_vec,s_matrix,params,e_E,e_I)
+% Solve the auditory cortex system of ODEs for an arbitrary train of
+% stimuli (s_matrix).
+%
+% s_matrix(onset_times, columns): Is a matrix with as many columns as
+% auditory cortex columns, and as many rows as different stimulus onsets.
+% dur_vec: vector of duration for each stimulus
 
+    %% Create neurons
+    E_ini = rand(params.N_E,params.columns);
+    I_ini = rand(params.N_I,params.columns);
+
+    %% Create synaptic resources
+    x_ini = rand(size(E_ini));
+    y_ini = rand(size(I_ini));
+
+    %% Get initial conditions
+    % set stimulus to 0 and let the system converge
+    X0 = [E_ini(:); I_ini(:); x_ini(:); y_ini(:)];
+    t_rest = 5*params.Tau_rec;
+    [t, X] = ode45(@(t,X) dXdt(t,X,params,0,e_E,e_I),[0,t_rest],X0);
+
+    [E,I,x,y] = reshape_X(X,[2,3,1], params);
+
+    E0 = E(:,:,end);
+    I0 = I(:,:,end);
+    x0 = x(:,:,end);
+    y0 = y(:,:,end);
+    
+    %% Solve ODE
+    
+    % if the starting time is different to 0, add a stimulus of 0 from t=0
+    % to until the starting time
+    dur_vec = dur_vec(:);
+    if start_time ~= 0
+        zero_stimulus = zeros(1,size(s_matrix,2));
+        s_matrix = cat(1,zero_stimulus,s_matrix);
+        dur_vec = cat(1,start_time,dur_vec);
+    end
+    
+    time_sum = 0;
+    E = [];
+    I = [];
+    x = [];
+    y = [];
+    t_vec = [];
+    next_E0 = E0;
+    next_I0 = I0;
+    next_x0 = x0;
+    next_y0 = y0;
+    for jj = 1:size(dur_vec,1)
+        t_span = [0, dur_vec(jj)];
+        s = s_matrix(jj,:);
+        X0 = [next_E0(:); next_I0(:); next_x0(:); next_y0(:)];
+        [t, X] = ode45(@(t,X) dXdt(t,X,params,s,e_E,e_I),t_span,X0);
+
+        t = t(:) + time_sum;
+        time_sum = time_sum + dur_vec(jj);
+        [E_jj,I_jj,x_jj,y_jj] = reshape_X(X,[1,3,2], params);% E(time,cols,N_E)
+        
+        E = cat(1,E,E_jj);
+        I = cat(1,I,I_jj);
+        x = cat(1,x,x_jj);
+        y = cat(1,y,y_jj);
+        t_vec = cat(1,t_vec,t);
+
+        % the last E,I,x,y of each stimulus, are the initial conditions for
+        % next one:
+        [E0_jj,I0_jj,x0_jj,y0_jj] = reshape_X(X,[2,3,1], params);
+        next_E0 = E0_jj(:,:,end);
+        next_I0 = I0_jj(:,:,end);
+        next_x0 = x0_jj(:,:,end);
+        next_y0 = y0_jj(:,:,end);
+           
+    end
+end
 function dXdt = dXdt(t,X,params,s,e_E,e_I)
     [E,I,x,y] = reshape_X_single_time_point(X,params);
    
@@ -115,7 +267,7 @@ function dXdt = dXdt(t,X,params,s,e_E,e_I)
     J_IE = params.J_IE;
     J_II = params.J_II;
     U = params.U;
-    Tau_rec = params.Taur_rec;
+    Tau_rec = params.Tau_rec;
     
     in_col_exci = sum(U*x.*E, 1);
     between_col_exci = zeros(5,columns);
@@ -228,4 +380,28 @@ function [E,I,x,y] = reshape_X(X,order,params)
     I = permute(I,order);
     x = permute(x,order);
     y = permute(y,order);
+end
+function s = compute_s_tuning_curve(s,s_params)
+    columns = size(s,2);
+    alpha = s_params.alpha;
+    lambda_c = s_params.lambda_c;
+    delta_left = s_params.delta_left;
+    delta_right = s_params.delta_right;
+    for tt = 1:size(s,1)
+        A_vec = s(tt,:);      
+        h = zeros(columns);
+        for M = 1:size(s,2)
+            for Q = 1:size(s,2)
+                A = A_vec(M); 
+                if Q > M
+                    delta = delta_right;
+                else
+                    delta = delta_left;
+                end
+                lambda_s = ((A-alpha)/delta)*heaviside(A-alpha-eps) + lambda_c;
+                h(M,Q) = A*exp(-abs(Q-M)/lambda_s);
+            end
+        end
+        s(tt,:) = sum(h,1);
+    end
 end
